fc7f880c4722e9b9b279b52bd22598803be2e2af
Chris Reinhold <chris.reinhold@bluechip-llc.com>
2017-09-01 16:12:26 -0400

    Updated comments in Tile Runner Files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareTileRunnerRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareTileRunnerRobot.java
@@ -11,17 +11,15 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
  * This is NOT an opmode.
  *
  * This class can be used to define all the specific hardware for a single robot.
- * In this case that robot is a Pushbot.
- * See PushbotTeleopTank_Iterative and others classes starting with "Pushbot" for usage examples.
+ * In this case that robot is a Tile Runner.
  *
  * This hardware class assumes the following device names have been configured on the robot:
  * Note:  All names are lower case and some have single spaces between words.
  *
- * Motor channel:  Left  drive motor:        "left_drive"
- * Motor channel:  Right drive motor:        "right_drive"
- * Motor channel:  Manipulator drive motor:  "left_arm"
- * Servo channel:  Servo to open left claw:  "left_hand"
- * Servo channel:  Servo to open right claw: "right_hand"
+ * Motor channel:  Left  drive motor1:        "left_drive1"
+ * Motor channel:  Left  drive motor2:        "left_drive2"
+ * Motor channel:  Right drive motor1:        "right_drive1"
+ * Motor channel:  Right drive motor2:        "right_drive2"
  */
 public class HardwareTileRunnerRobot
 {
